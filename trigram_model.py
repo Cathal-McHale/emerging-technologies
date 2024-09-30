9a143c4dcb3cd9fe0a0061d15038561b9fc9a7f3
cathal McHale <123738525+Cathal-McHale@users.noreply.github.com>
2024-09-30 16:37:31 +0000

    Remove any preamble and postamble test 1

--- trigram_model.py
+++ trigram_model.py
@@ -4,9 +4,16 @@ from collections import defaultdict#import defaultdict for default value
 
 def cleanup_text(text):
     #remove all non-letter characters except periods and spaces and convert to uppercase
-    cleaned_txt = re.sub(r'[A-Za-z]', '', text).upper()
+    cleaned_txt = re.sub(r'[^A-Za-z. ]', '', text).upper()
     #replace multiple spaces with a single space
     cleaned_txt = re.sub(r'\s+', ' ', cleaned_txt)
 
+    return cleaned_txt
 
-    return cleanup_text
+test_text = "Hello, world! 123444 testing to see if it removes"
+cleaned_txt = cleanup_text(test_text)
+
+print("original text = : ", test_text)
+print("Cleaned text = : ", cleaned_txt)
+
+   # 
